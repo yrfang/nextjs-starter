9635c1c5bbbe75cdabb81f94891a2efdcbb97ab5
YR Fang <fyr1116@gmail.com>
2018-06-27 21:20:12 +0800

    fetch data without saga

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,23 @@
+import React from 'react';
+import 'isomorphic-unfetch';
 import Count from 'components/Count';
 
-const Index = () => (
-  <div>
-    <Count />
-  </div>
-);
+class Index extends React.Component {
+  static async getInitialProps() {
+    // eslint-disable-next-line no-undef
+    const res = await fetch('https://api.github.com/repos/zeit/next.js')
+    const json = await res.json()
+    return { stars: json.stargazers_count }
+  }
+
+  render() {
+    return (
+      <div>
+        <Count />
+        <p>Next.js has <mark>{this.props.stars}</mark> ⭐️</p>
+      </div>
+    )
+  }
+}
 
 export default Index;
